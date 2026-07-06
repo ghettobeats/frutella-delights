5ded3fb1174b573d47f2f7fdc8a6f1cc82f3550f
root <webmaster@mitur.gob.do>
2026-07-05 21:09:46 -0400

    [FIX] Mejoras en módulo frutella - seguridad, stock, ventas y producción

--- frutella/models/production.py
+++ frutella/models/production.py
@@ -5,10 +5,10 @@ from odoo.exceptions import ValidationError
 class MrpProduction(models.Model):
     _inherit = 'mrp.production'
 
-    def _post_inventory(self, cancel_backorder=False):
+    def action_confirm(self):
         for order in self:
             for move in order.move_raw_ids.filtered(
-                lambda m: m.state not in ('done', 'cancel') and m.product_uom_qty > 0
+                lambda m: m.product_uom_qty > 0
             ):
                 if move.product_id.qty_available < move.product_uom_qty:
                     raise ValidationError(
@@ -20,4 +20,7 @@ class MrpProduction(models.Model):
                             move.product_id.qty_available,
                         )
                     )
+        return super().action_confirm()
+
+    def _post_inventory(self, cancel_backorder=False):
         return super()._post_inventory(cancel_backorder=cancel_backorder)
